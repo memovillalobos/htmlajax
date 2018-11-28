599d6437e5e83cbbadf0bec77bc7d55eab54cb17
Guillermo Villalobos <guillermo@pixan.io>
2018-11-28 15:03:31 -0600

    Save progress

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Obtener la referencia al elemento
 var btnActualizar = document.getElementById("btnActualizar");
+var btnGuardar = document.getElementById("btnGuardar");
 
 // Agregar un event listener para cada click
 btnActualizar.addEventListener('click', actualizar);
+btnGuardar.addEventListener('click', guardar);
 
 actualizar();
 
@@ -53,4 +55,31 @@ function actualizar()
   };
   xhttp.open("GET", "http://nyc.pixan.io/ajax/public/api/students", true);
   xhttp.send();
+}
+
+function guardar()
+{
+
+  var first_name = document.getElementById('first_name').value;
+  var last_name = document.getElementById('last_name').value;
+  var email = document.getElementById('email').value;
+  var phone_numer = document.getElementById('phone_number').value;
+
+  var data = new FormData();
+  data.append('first_name',   first_name);
+  data.append('last_name',    last_name);
+  data.append('email',        email);
+  data.append('phone_number',  phone_numer);
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function ()
+  {
+    if(this.readyState == 4 && this.status == 200)
+    {
+      // var response = JSON.parse(this.responseText);
+      alert(this.responseText);
+    }
+  };
+  xhttp.open("POST", "http://nyc.pixan.io/ajax/public/api/students", true);
+  xhttp.send(data);
 }
