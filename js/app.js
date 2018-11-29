72e748b425f7c04c30a2824035499bac8c50c3a7
Guillermo Villalobos <guillermo@pixan.io>
2018-11-29 09:07:41 -0600

    Se agregó boton de eliminar

--- js/app.js
+++ js/app.js
@@ -22,33 +22,43 @@ function actualizar()
         document.getElementsByTagName('tbody')[0].innerHTML = "";
         response.students.forEach(function(student)
         {
-          var html = "<tr><td>"
-            + student.id
-            + "</td><td>"
-            + student.first_name
-            + "</td><td>"
-            + student.last_name
-            + "</td></tr>";
-          document.getElementsByTagName('tbody')[0].innerHTML += html;
+          // var html = "<tr><td>"
+          //   + student.id
+          //   + "</td><td>"
+          //   + student.first_name
+          //   + "</td><td>"
+          //   + student.last_name
+          //   + "</td><td>"
+          //   + "<button class='Eliminar' type='button'>Eliminar</button>"
+          //   + "</td></tr>";
+          // document.getElementsByTagName('tbody')[0].innerHTML += html;
 
-          // var row = document.createElement("tr");
-          // var idCell = document.createElement("td");
-          // var firstNameCell = document.createElement("td");
-          // var lastNameCell = document.createElement("td");
-          //
-          // var idText = document.createTextNode(student.id);
-          // var firstNameText = document.createTextNode(student.first_name);
-          // var lastNameText = document.createTextNode(student.last_name);
-          //
-          // idCell.appendChild(idText);
-          // firstNameCell.appendChild(firstNameText);
-          // lastNameCell.appendChild(lastNameText);
-          //
-          // row.appendChild(idCell);
-          // row.appendChild(firstNameCell);
-          // row.appendChild(lastNameCell);
+          var row = document.createElement("tr");
+          var idCell = document.createElement("td");
+          var firstNameCell = document.createElement("td");
+          var lastNameCell = document.createElement("td");
+          var deleteButtonCell = document.createElement("td");
+          var deleteButton = document.createElement("button");
+          deleteButton.className = "Eliminar";
 
-          //document.getElementsByTagName('tbody')[0].appendChild(row);
+          var idText = document.createTextNode(student.id);
+          var firstNameText = document.createTextNode(student.first_name);
+          var lastNameText = document.createTextNode(student.last_name);
+          var deleteButtonText = document.createTextNode("Eliminar");
+
+          idCell.appendChild(idText);
+          firstNameCell.appendChild(firstNameText);
+          lastNameCell.appendChild(lastNameText);
+
+          deleteButton.appendChild(deleteButtonText);
+          deleteButtonCell.appendChild(deleteButton);
+
+          row.appendChild(idCell);
+          row.appendChild(firstNameCell);
+          row.appendChild(lastNameCell);
+          row.appendChild(deleteButtonCell);
+
+          document.getElementsByTagName('tbody')[0].appendChild(row);
         });
       }
     }
